bc2cf91f2f36cfcfcbb47d6d425ebda572645205
rafa133169 <127886555+rafa133169@users.noreply.github.com>
2024-02-05 15:07:45 -0500

    quedo al 100 con rama2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './componentes/paginas/login';
-import Modal from './componentes/paginas/modal';
-import Header from './componentes/header';
-import UsersManagement from './componentes/paginas/admin';
-import SearchComponent from './componentes/buscador';
-import ListaTrabajos from './componentes/listaTrabajos';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./componentes/paginas/login";
+import Modal from "./componentes/paginas/modal";
+import Header from "./componentes/header";
+import UsersManagement from "./componentes/paginas/admin";
+import SearchComponent from "./componentes/buscador";
+import ListaTrabajos from "./componentes/listaTrabajos";
+import { ProtectedRoute, ProtectedRouteLogin } from "./ProtectedRoute";
 
 function App() {
   const [showModal, setShowModal] = useState(false);
@@ -17,31 +18,41 @@ function App() {
     <Router>
       <div>
         <Routes>
-        <Route element={<ProtectedRouteLogin />} >
-          <Route path="/" element={<Login />} />
-          
-          <Route
-            path="/Home/:id"
-            element={
-              <div className="p-0 ">
-                <Header />
-                <h1 className='font-bold  m-8 float-left font- text-2xl '>LISTA DE TRABAJOS</h1>
-                <p className='float-left text-slate-500 text-justify'>Haz click en el boton de “Nuevo” para agregar un trabajo a la lista<br></br>
-Haz click en un trabajo existente para ver los detalles o actualizar la información</p>
-                <Modal showModal={showModal} closeModal={closeModal} />
-                <ListaTrabajos />
-              </div>
-            }
-          />
+          <Route element={<ProtectedRouteLogin />}>
+            <Route path="/" element={<Login />} />
+          </Route>
+          <Route element={<ProtectedRoute/>}>
+            <Route
+              path="/Home/:id"
+              element={
+                <div className="p-0 ">
+                  <Header />
+                  <h1 className="font-bold  m-8 float-left font- text-2xl ">
+                    LISTA DE TRABAJOS
+                  </h1>
+                  <p className="float-left text-slate-500 text-justify">
+                    Haz click en el boton de “Nuevo” para agregar un trabajo a
+                    la lista<br></br>
+                    Haz click en un trabajo existente para ver los detalles o
+                    actualizar la información
+                  </p>
+                  <Modal showModal={showModal} closeModal={closeModal} />
+                  <ListaTrabajos />
+                </div>
+              }
+            />
 
-          {/* Nueva ruta para la gestión de usuarios */}
-          <Route path="/users/:id" element={
-           <div className="p-4">
-            <Header />
-          <UsersManagement />
-          </div>
-          } />
-
+            {/* Nueva ruta para la gestión de usuarios */}
+            <Route
+              path="/users/:id"
+              element={
+                <div className="p-4">
+                  <Header />
+                  <UsersManagement />
+                </div>
+              }
+            />
+          </Route>
         </Routes>
       </div>
     </Router>

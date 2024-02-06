6bf70141152235a247464e42cde7499611645d5b
tommoko3ds <kenyaescamill@gmail.com>
2024-02-06 01:12:51 -0500

    editar preciooz

--- src/componentes/listaTrabajos.jsx
+++ src/componentes/listaTrabajos.jsx
@@ -5,6 +5,8 @@ import { faEdit, faEye, faTrashAlt, faPenToSquare } from "@fortawesome/free-soli
 
 const ListaTrabajos = () => {
   const [trabajos, setTrabajos] = useState([]);
+  const [precioTotalOriginal, setPrecioTotalOriginal] = useState("");
+
   const [editarModal, setEditarModal] = useState(false);
   const [trabajoSeleccionado, setTrabajoSeleccionado] = useState(null);
   const [tituloEdit, setTituloEdit] = useState("");
@@ -13,10 +15,11 @@ const ListaTrabajos = () => {
   const [estatusEdit, setEstatusEdit] = useState("");
   const [horasEdit, setHorasEdit] = useState("");
   const [precioMaterialesEdit, setPrecioMaterialesEdit] = useState("");
-  const[precioTotalEdit, setPrecioTotalEdit] = useState("");
-  const[tipoMaterialEdit, setTipoMaterialEdit] = useState("");
-    const [expandedDetails, setExpandedDetails] = useState({});
-
+  const [precioTotalEdit, setPrecioTotalEdit] = useState("");
+  const [tipoMaterialEdit, setTipoMaterialEdit] = useState("");
+  const [tipoTrabajo, setTipoTrabajo] = useState('Reparacion Mecanica');
+  const [expandedDetails, setExpandedDetails] = useState({});
+  const [filtro, setFiltro] = useState("");
 
   useEffect(() => {
     fetch("http://localhost:5000/api/jobs")
@@ -47,16 +50,25 @@ const ListaTrabajos = () => {
     setEstatusEdit(trabajoSeleccionado.estatus);
     setHorasEdit(trabajoSeleccionado.horas);
     setPrecioMaterialesEdit(trabajoSeleccionado.precioMateriales);
+    setPrecioTotalEdit(trabajoSeleccionado.precioTotal);
+    setPrecioTotalOriginal(trabajoSeleccionado.precioTotal); // Guarda el precio original
     setEditarModal(true);
   };
   
+  const handleTipoTrabajoChange = (event) => {
+    setTipoEdit(event.target.value); // Cambia setTipoTrabajo a setTipoEdit
+  };
+
+  
   const handleUpdateTrabajo = async () => {
     try {
-      if (!tituloEdit ) {
+      if (!tituloEdit) {
         alert("Por favor, complete todos los campos.");
         return;
       }
-
+  
+      const nuevoPrecio = NuevoPrecio(); // Calcula el nuevo precio
+  
       const response = await axios.put(
         `http://localhost:5000/api/jobs/${trabajoSeleccionado.id_trabajo}`,
         {
@@ -66,13 +78,12 @@ const ListaTrabajos = () => {
           tipo: tipoEdit,
           horas: horasEdit,
           precioMateriales: precioMaterialesEdit,
-          precioTotal: precioTotalEdit,
+          precioTotal: nuevoPrecio, // Actualiza el precioTotal con el nuevo precio calculado
         }
       );
+  
       if (response.status === 200) {
-        const updatedResponse = await axios.get(
-          "http://localhost:5000/api/jobs"
-        );
+        const updatedResponse = await axios.get("http://localhost:5000/api/jobs");
         setTrabajos(updatedResponse.data);
         setTituloEdit("");
         setDescripcionEdit("");
@@ -87,10 +98,7 @@ const ListaTrabajos = () => {
       console.error("Error de red al actualizar trabajo:", error);
     }
   };
-
- 
-  const trabajosEnProceso = trabajos.filter((trabajo) => trabajo.estatus === "En proceso");
-  const trabajosTerminados = trabajos.filter((trabajo) => trabajo.estatus === "Terminado");
+  
 
 
   const handleViewDetails = (id_trabajo) => {
@@ -107,15 +115,76 @@ const ListaTrabajos = () => {
     }));
   };
 
-  return (
+  const filtrarTrabajos = () => {
+    if (!filtro) {
+      return trabajos;
+    }
+
+    return trabajos.filter((trabajo) =>
+      trabajo.titulo.toLowerCase().includes(filtro.toLowerCase())
+    );
+  };
+
+  const trabajosEnProceso = Array.isArray(trabajos)
+    ? filtrarTrabajos().filter((trabajo) => trabajo.estatus === "En proceso")
+    : [];
+
+  const trabajosTerminados = Array.isArray(trabajos)
+    ? filtrarTrabajos().filter((trabajo) => trabajo.estatus === "Terminado")
+    : [];
+
+    const NuevoPrecio = () => {
+      const horasEditFloat = parseFloat(horasEdit);
+      const precioMaterialesEditFloat = parseFloat(precioMaterialesEdit);
+  
+      let precioTipoTrabajo = 0;
+  
+      switch (tipoEdit) {
+        case 'Reparacion Mecanica':
+          precioTipoTrabajo = horasEditFloat * 350 + precioMaterialesEditFloat * 1.1;
+          break;
+        case 'Reparacion Chapa y Pintura':
+          precioTipoTrabajo = horasEditFloat * 350 + precioMaterialesEditFloat * 1.3;
+          break;
+        case 'Revision':
+          precioTipoTrabajo = horasEditFloat * 350 + 450;
+          break;
+        default:
+          break;
+      }
+  
+      return precioTipoTrabajo;
+    };
+   
+  
+
+
+return (
     <div>
+      <div className="mb-4">
+      <input
+  type="text"
+  placeholder="Buscar trabajo por titulo..."
+  value={filtro}
+  onChange={(e) => setFiltro(e.target.value)}
+  className="w-64 p-2 border rounded" 
+/>
+
+    <button
+      className="bg-blue-500 text-white px-4 py-2 ml-2 rounded"
+      onClick={() => setFiltro("")}
+    >
+      Cancelar
+    </button>
+  
+  </div>
       <div className="flex">
         <div className="w-1/2 pr-4">
           <h3 className="text-lg font-bold mb-2 bg-orange-300 rounded-lg p-2 w-fit">
             En proceso
           </h3>
           <ul>
-             {trabajosEnProceso.map((trabajo) => (
+            {trabajosEnProceso.map((trabajo) => (
               <li key={trabajo.id_trabajo} className="mb-4 border p-4 rounded shadow-md flex justify-between items-center">
                 <div>
                   <h4 className="text-lg font-bold">{trabajo.titulo}</h4>
@@ -206,6 +275,32 @@ const ListaTrabajos = () => {
             onChange={(e) => setPrecioMaterialesEdit(e.target.value)}
             className="w-full p-2 border rounded"
           />
+
+        </div>
+        <div>
+        <label htmlFor="tipoTrabajo" className="block text-sm font-medium text-gray-600">
+          Tipo de Trabajo:
+        </label>
+        <select
+          id="tipoTrabajo"
+          value={tipoEdit}
+          onChange={handleTipoTrabajoChange}
+          className="mt-1 p-2 border rounded w-full"
+        >
+          <option value="Reparacion Mecanica">Reparación Mecánica</option>
+          <option value="Reparacion Chapa y Pintura">Reparación Chapa y Pintura</option>
+          <option value="Revision">Revisión</option>
+        </select>
+      </div>
+        <div>
+          <label htmlFor="precioTotal">Precio Total:</label>
+          <input
+            type="number"
+            id="precioTotal"
+            value={precioTotalEdit}
+            onChange={(e) => setPrecioMaterialesEdit(e.target.value)}
+            className="w-full p-2 border rounded"
+          />
         </div>
       </div>
       <button
@@ -224,12 +319,6 @@ const ListaTrabajos = () => {
   </div>
 )}
 
-
-
-
-
-
-
   </li>
 ))}
           </ul>
@@ -239,7 +328,7 @@ const ListaTrabajos = () => {
             Terminados
           </h3>
           <ul>
-          {trabajosTerminados.map((trabajo) => (
+            {trabajosTerminados.map((trabajo) => (
   <li key={trabajo.id_trabajo} className="mb-4 border shadow-md p-4 rounded flex justify-between items-center">
     <div>
       <h4 className="text-lg font-bold">{trabajo.titulo}</h4>
@@ -330,6 +419,16 @@ const ListaTrabajos = () => {
             type="number"
             id="precioMaterialesEdit"
             value={precioMaterialesEdit}
+            onChange={(e) => setPrecioMaterialesEdit(e.target.value)}
+            className="w-full p-2 border rounded"
+          />
+        </div>
+        <div>
+          <label htmlFor="precioTotal">Precio Total:</label>
+          <input
+            type="number"
+            id="precioTotal"
+            value={precioTotalEdit}
             onChange={(e) => setPrecioMaterialesEdit(e.target.value)}
             className="w-full p-2 border rounded"
           />
